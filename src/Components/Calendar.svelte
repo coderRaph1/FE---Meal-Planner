84850ee52c9cf1b16e73339f0842a54a78c16d19
CoderRaph1 <raphael02@hotmail.co.uk>
2024-08-20 15:59:20 +0100

    reload page with correct dates on screen and url when user clicks a date. and changed the nav title and image link

--- src/Components/Calendar.svelte
+++ src/Components/Calendar.svelte
@@ -1,87 +1,94 @@
 <script>
-	import AddMeal from "./Add-meal.svelte";
+    import { onMount } from 'svelte'
+	import AddMeal from './Add-meal.svelte'
 
+    let selectedDay = null
+    let weekDates = [];
+
+    // grabs selectedDay from URL
+    function getSelectedDayFromUrl() {
+        const urlParams = new URLSearchParams(window.location.search)
+        console.log(window)
+        return urlParams.get('selectedDay')
+    }
+
+    // Function to set selected day and reload the page
+    function selectDay(day) {
+        window.location.href = `?selectedDay=${day}`
+    }
+
+    // Function to generate a week's worth of dates
+    function generateWeekDates() {
+        const today = new Date()
+        const weekStart = today.getDate() - today.getDay()
+        const week = []
+
+        for (let i = 0; i < 7; i++) {
+            const date = new Date(today)
+            date.setDate(weekStart + i)
+            week.push({
+                day: date.getDate(),
+                weekday: date.toLocaleDateString('en-US', { weekday: 'short' }),
+                dateStr: date.toISOString().slice(0, 10) 
+            });
+        }
+
+        return week;
+    }
+
+    onMount(() => {
+        selectedDay = getSelectedDayFromUrl()
+        weekDates = generateWeekDates()
+    })
 </script>
-<div class="h-screen bg-gray-100 p-6">
-    <!-- { /*variation dark set*/ } -->
-  
-     <!-- { /*variation dark set*/ } -->
-     <div  class='flex bg-white shadow-md justify-start md:justify-center rounded-lg overflow-x-scroll mx-auto py-4 px-2  md:mx-12'>
-       
-         <div class='flex group hover:bg-purple-500 hover:shadow-lg hover-dark-shadow rounded-lg mx-1 transition-all	duration-300	 cursor-pointer justify-center w-16'>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-900 group-hover:text-gray-100 text-sm transition-all	duration-300'> Sun </p>
-                    <p class='text-gray-900 group-hover:text-gray-100 mt-3 group-hover:font-bold transition-all	duration-300'> 11 </p>
-                 </div>
-             </div>
-         </div>
-       
-       <div class='flex group hover:bg-purple-500 hover:shadow-lg hover-dark-shadow rounded-lg mx-1 transition-all	duration-300	 cursor-pointer justify-center  w-16'>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-900 group-hover:text-gray-100 text-sm transition-all	duration-300'> Mon </p>
-                    <p class='text-gray-900 group-hover:text-gray-100 mt-3 group-hover:font-bold transition-all	duration-300'> 12 </p>
-                 </div>
-             </div>
-         </div>
-       
-       <div class='flex group hover:bg-purple-500 hover:shadow-lg hover-dark-shadow rounded-lg mx-1 transition-all	duration-300	 cursor-pointer justify-center  w-16'>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-900 group-hover:text-gray-100 text-sm transition-all	duration-300'> Tue </p>
-                    <p class='text-gray-900 group-hover:text-gray-100 mt-3 group-hover:font-bold transition-all	duration-300'> 13</p>
-                 </div>
-             </div>
-         </div>
-       
-         <div class='flex group bg-purple-600 shadow-lg dark-shadow rounded-lg mx-1 cursor-pointer justify-center relative  w-16'>
-           <span class="flex h-3 w-3 absolute -top-1 -right-1">
-             <span class="animate-ping absolute group-hover:opacity-75 opacity-0 inline-flex h-full w-full rounded-full bg-purple-400 "></span>
-             <span class="relative inline-flex rounded-full h-3 w-3 bg-purple-100"></span>
-           </span>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-100 text-sm'> Wed </p>
-                    <p class='text-gray-100  mt-3 font-bold'> 14 </p>
-                 </div>
-             </div>
-         </div>
-       
-       <div class='flex group hover:bg-purple-500 hover:shadow-lg hover-dark-shadow rounded-lg mx-1 transition-all	duration-300 cursor-pointer justify-center w-16'>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-900 group-hover:text-gray-100 text-sm transition-all	duration-300'> Thu </p>
-                    <p class='text-gray-900 group-hover:text-gray-100 mt-3 group-hover:font-bold transition-all	duration-300'> 15 </p>
-                 </div>
-             </div>
-         </div>
-       
-       <div class='flex group hover:bg-purple-500 hover:shadow-lg hover-dark-shadow rounded-lg mx-1 transition-all	duration-300	 cursor-pointer justify-center  w-16'>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-900 group-hover:text-gray-100 text-sm transition-all	duration-300'> Fri </p>
-                    <p class='text-gray-900 group-hover:text-gray-100 mt-3 group-hover:font-bold transition-all	duration-300'> 16 </p>
-                 </div>
-             </div>
-         </div>
-         
-       <div class='flex group hover:bg-purple-500 hover:shadow-lg hover-dark-shadow rounded-lg mx-1 transition-all	duration-300	 cursor-pointer justify-center w-16'>
-             <div class='flex items-center px-4 py-4'>
-                 <div class='text-center'>
-                    <p class='text-gray-900 group-hover:text-gray-100 text-sm transition-all	duration-300'> Sat </p>
-                    <p class='text-gray-900 group-hover:text-gray-100 mt-3 group-hover:font-bold transition-all	duration-300'> 17 </p>
-                 </div>
-             </div>
-         </div>
-         
-       
-     </div>
-     
-     <br/><br/>
-     
-     
-   </div>
+
+<style>
+    .calendar {
+        display: flex;
+        justify-content: center;
+        gap: 8px;
+        margin-top: 20px;
+        flex-wrap: wrap;
+    }
+
+    .day {
+        cursor: pointer;
+        transition: all 0.3s;
+        padding: 16px;
+        text-align: center;
+        border-radius: 8px;
+        margin: 4px;
+        width: 60px;
+    }
+
+    .day.selected {
+        background-color: #6b46c1; 
+        color: #f7fafc; 
+        font-weight: bold;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .day:hover {
+        background-color: #6b46c1;
+        color: #f7fafc;
+    }
+
+    @media (max-width: 600px) {
+        .day {
+            width: 50px;
+        }
+    }
+</style>
+
+
+<div id="calendar" class="calendar">
+    {#each weekDates as date}
+        <div id="day-{date.day}" class="day {selectedDay === date.dateStr ? 'selected' : ''}" on:click={() => selectDay(date.dateStr)}>
+            <p>{date.weekday}</p>
+            <p>{date.day}</p>
+        </div>
+    {/each}
+</div>
 
 
    <AddMeal/>
