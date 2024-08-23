671077e62f235a7a2213630f0ba65f5a482966b5
CoderRaph1 <raphael02@hotmail.co.uk>
2024-08-23 11:44:40 +0100

    update routing to home (when you press sign out)

--- src/Components/Navbar.svelte
+++ src/Components/Navbar.svelte
@@ -63,7 +63,7 @@
 					</li>
 					<li>
 						<a
-							href="/sign-out"
+							href="/"
 							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
 							>Sign out</a
 						>
