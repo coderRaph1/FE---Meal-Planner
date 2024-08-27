3e8cf59b3ea60850db9725fb795dbffac0af852b
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-27 09:41:24 +0100

    add changes to shopping list page

--- src/routes/shopping-list/+page.svelte
+++ src/routes/shopping-list/+page.svelte
@@ -1,21 +1,22 @@
 <script>
-	import { onMount } from 'svelte';
+	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
 	import { userDetails } from '../../stores.js';
 	import { getListsForUser, postNewList, postNewListByUSerId } from '../../api.js';
-	import Modal from "../../Components/Modal.svelte"
+	import Modal from '../../Components/Modal.svelte';
 
 	let userLists = [];
-	let showModal = false
-	onMount(() => {
+
+	let showModal = false;
+	beforeUpdate(() => {
 		getListsForUser($userDetails.user.user_id).then((data) => {
 			userLists = data.lists;
-			//console.log(data, 'data');
-			//console.log(userLists,'<<userlists');
 		});
 	});
-	
-
-	
+	afterUpdate(() => {
+		getListsForUser($userDetails.user.user_id).then((data) => {
+			userLists = data.lists;
+		});
+	});
 </script>
 
 <h1>My Lists</h1>
@@ -23,11 +24,12 @@
 	data-modal-target="default-modal"
 	data-modal-toggle="default-modal"
 	class="block rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-	type="button" on:click={() => showModal = !showModal}>Add a list</button
+	type="button"
+	on:click={() => (showModal = !showModal)}>Add a list</button
 >
 <ul>
 	{#each userLists as list}
-		<li>{list.list_name}</li>
+		<button style="display: block;">{list.list_name}</button>
 	{/each}
 </ul>
 <!-- <label for="newListName">List name</label>
@@ -35,4 +37,4 @@
 <label for="isPrivate">Private</label>
 <input type="checkbox" bind:checked={isPrivate} />
 <button on:click={handleClick}>Add new List</button> -->
-<Modal show={showModal} userLists={userLists}/>
+<Modal show={showModal} {userLists} />
