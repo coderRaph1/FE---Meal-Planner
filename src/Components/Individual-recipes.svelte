6a9ea3b49e71245d638168e7d48013354155f844
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-28 09:59:05 +0100

    add code to delete a list

--- src/Components/Individual-recipes.svelte
+++ src/Components/Individual-recipes.svelte
@@ -1,109 +1,139 @@
 <script>
+	import { onMount } from 'svelte';
+	import { getMealById } from '../utils/api';
+	import { page } from '$app/stores';
+	import { getListsForUser, postItem } from '../api';
+	import { userDetails, userLists } from '../stores';
 
-  import { onMount } from "svelte";
-  import { getMealById } from "../utils/api";
-  import { page } from '$app/stores';
-	import { getListsForUser, postItem } from "../api";
-	import { userDetails, userLists } from "../stores";
+	let currentPath = '/';
+	let mealData = {};
+	let selectedIngredients = [];
 
-  let currentPath = '/';
-  let mealData = {};
-  let selectedIngredients = [];
+	$: currentPath = $page.url.pathname.split('/').filter(Boolean).pop() || '/';
 
-  $: currentPath = $page.url.pathname.split('/').filter(Boolean).pop() || '/';
-  
-  let usersList = []
+	let usersList = [];
 
-  onMount(() => {
-    getMealById(currentPath).then((data) => {
-      mealData = data[0];
-      console.log($userDetails, 'user details');
-      return getListsForUser($userDetails.user.user_id)
-    }).then((data) => {
-      usersList = data.lists
-      console.log(usersList, 'the different lists available');
-    })
-  });
+	onMount(() => {
+		getMealById(currentPath)
+			.then((data) => {
+				mealData = data[0];
+				console.log($userDetails, 'user details');
+				return getListsForUser($userDetails.user.user_id);
+			})
+			.then((data) => {
+				usersList = data.lists;
+				console.log(usersList, 'the different lists available');
+			});
+	});
 
-  function getIngredients(meal) {
-    return Object.keys(meal)
-      .filter(key => key.startsWith('strIngredient') && meal[key])
-      .map(key => meal[key]);
-  }
+	function getIngredients(meal) {
+		return Object.keys(meal)
+			.filter((key) => key.startsWith('strIngredient') && meal[key])
+			.map((key) => meal[key]);
+	}
 
-  function handleCheckboxChange(event, ingredient) {
-    if (event.target.checked) {
-      selectedIngredients.push(ingredient);
-    } else {
-      selectedIngredients = selectedIngredients.filter(item => item !== ingredient);
-    }
-  }
+	function handleCheckboxChange(event, ingredient) {
+		if (event.target.checked) {
+			selectedIngredients.push(ingredient);
+		} else {
+			selectedIngredients = selectedIngredients.filter((item) => item !== ingredient);
+		}
+	}
 
-  function addToShoppingList(){
+	function addToShoppingList() {
+		loading = true;
+		console.log(selectedList);
+		const promiseArr = selectedIngredients.map((itemName) => {
+			return postItem(selectedList, itemName, 1);
+		});
 
-    loading = true
+		console.log(selectedIngredients, 'items in the checked-box array');
+		return Promise.all(promiseArr).then((data) => {
+			console.log(data);
+			loading = false;
+			confirmed = true;
+		});
+	}
 
-    selectedIngredients.map((itemName) => {
-      console.log(selectedList, 'selected list')
-      return postItem(selectedList, itemName, 1)
-      .then(() => {
-        loading = false
-        confirmed = true
-      })
-    })
-    
-    console.log(selectedIngredients, 'items in the checked-box array')
-  }
+	let selectedList;
+	$: console.log(selectedList);
 
-  let selectedList 
-  $: console.log(selectedList)
-
-  let loading = false
-  let confirmed = false
-
-
+	let loading = false;
+	let confirmed = false;
 </script>
-<div class="flex justify-center items-center min-h-screen bg-gray-100">
-  <div class="w-full max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-<a href="#" class="w-full">
-      <img class="w-full h-1/3 object-cover rounded-t-lg" src={mealData.strMealThumb} alt="Recipe Image" />
-    </a>
-    <div class="p-8 flex-1">
-      <a href="#">
-        <h5 class="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Ingredients needed:</h5>
-      </a>
-      <ul class="mb-8 font-normal text-lg text-gray-700 dark:text-gray-400">
-        {#each getIngredients(mealData) as ingredient}
-          <li>
-            <label class="flex items-center">
-              <input type="checkbox" class="mr-3" on:change="{(event) => handleCheckboxChange(event, ingredient)}">
-              {ingredient}
-            </label>
-          </li>
-        {/each}
-      </ul>
-      <form>
-        <label for="list-name">Choose a list to add items to:</label>
-        <select id='list-name' bind:value={selectedList}>
-         {#each usersList as listItem} <option value={listItem.list_id}>{listItem.list_name}</option>{/each}
 
-        </select>
-      </form>
-      <h5 class="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Instructions:</h5>
-      <p class="mb-8 font-normal text-lg text-gray-700 dark:text-gray-400">
-        {mealData.strInstructions}
-      </p>
-    </div>
-    <div class="p-8">
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-7 py-3 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full" on:click="{addToShoppingList}">
-        Add Items to Shopping List
-        <svg class="rtl:rotate-180 w-4 h-4 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </button>
-      {#if loading}<p>Loading...</p> 
-      {:else if confirmed} <p>Your Shopping List has been Updated</p> 
-      {/if}
-    </div>
-  </div>
+<div class="flex min-h-screen items-center justify-center bg-gray-100">
+	<div
+		class="w-full max-w-lg rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800"
+	>
+		<a href="#" class="w-full">
+			<img
+				class="h-1/3 w-full rounded-t-lg object-cover"
+				src={mealData.strMealThumb}
+				alt="Recipe Image"
+			/>
+		</a>
+		<div class="flex-1 p-8">
+			<a href="#">
+				<h5 class="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+					Ingredients needed:
+				</h5>
+			</a>
+			<ul class="mb-8 text-lg font-normal text-gray-700 dark:text-gray-400">
+				{#each getIngredients(mealData) as ingredient}
+					<li>
+						<label class="flex items-center">
+							<input
+								type="checkbox"
+								class="mr-3"
+								on:change={(event) => handleCheckboxChange(event, ingredient)}
+							/>
+							{ingredient}
+						</label>
+					</li>
+				{/each}
+			</ul>
+			<form>
+				<label for="list-name">Choose a list to add items to:</label>
+				<select id="list-name" bind:value={selectedList}>
+					{#each usersList as listItem}
+						<option value={listItem.list_id}>{listItem.list_name}</option>{/each}
+				</select>
+			</form>
+			<h5 class="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+				Instructions:
+			</h5>
+			<p class="mb-8 text-lg font-normal text-gray-700 dark:text-gray-400">
+				{mealData.strInstructions}
+			</p>
+		</div>
+		<div class="p-8">
+			<button
+				type="button"
+				class="inline-flex w-full items-center rounded-lg bg-blue-700 px-7 py-3 text-center text-lg font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				on:click={addToShoppingList}
+			>
+				Add Items to Shopping List
+				<svg
+					class="ms-3 h-4 w-4 rtl:rotate-180"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 14 10"
+				>
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M1 5h12m0 0L9 1m4 4L9 9"
+					/>
+				</svg>
+			</button>
+			{#if loading}<p>Loading...</p>
+			{:else if confirmed}
+				<p>Your Shopping List has been Updated</p>
+			{/if}
+		</div>
+	</div>
 </div>
--- src/routes/shopping-list/+page.svelte
+++ src/routes/shopping-list/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
 	import { listData, userDetails, userLists } from '../../stores.js';
-	import { getListById, getListsForUser, postNewList, postNewListByUSerId } from '../../api.js';
+	import { deleteListFromAUser, getListById, getListsForUser } from '../../api.js';
 	import Modal from '../../Components/Modal.svelte';
 	import ListItems from '../../Components/ListItems.svelte';
+	import Navbar from '../../Components/Navbar.svelte';
 
 	let showModal = false;
+	let isErr = false;
 	onMount(() => {
 		getListsForUser($userDetails.user.user_id).then((data) => {
 			userLists.update(() => data.lists);
@@ -17,9 +19,29 @@
 			listData.update(() => data.list);
 		});
 	}
+	let showEdit = 'hide';
+	function handleEditClick() {
+		showEdit = showEdit === 'hide' ? 'show' : 'hide';
+	}
+
+	function deleteList(index) {
+		let curr = [...$userLists];
+
+		userLists.update((curr) => {
+			console.log(curr);
+			curr.splice(index, 1);
+			return curr;
+		});
+		console.log(curr);
+		console.log(curr[index].list_id);
+		return deleteListFromAUser($userDetails.user.user_id, curr[index].list_id).catch((err) => {
+			isErr = true;
+			userLists.update(() => curr);
+		});
+	}
 </script>
 
-<div></div>
+<Navbar />
 <h1>My Lists</h1>
 <button
 	data-modal-target="default-modal"
@@ -28,16 +50,32 @@
 	type="button"
 	on:click={() => (showModal = !showModal)}>Add a list</button
 >
+<button
+	data-modal-target="default-modal"
+	data-modal-toggle="default-modal"
+	class="block rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+	type="button"
+	on:click={handleEditClick}>Edit lists</button
+>
 <ul>
-	{#each $userLists as list}
+	{#each $userLists as list, index}
 		<button
 			style="display: block;"
 			on:click={() => {
 				handleClick(list.list_id);
 			}}>{list.list_name}</button
 		>
+		<button
+			class={showEdit}
+			on:click={() => {
+				deleteList(index);
+			}}>Delete list</button
+		>
 	{/each}
 </ul>
+{#if isErr}
+	<p>An error has occured</p>
+{/if}
 <!-- <label for="newListName">List name</label>
 <input type="text" id="newListName" bind:value={list_name} />
 <label for="isPrivate">Private</label>
@@ -47,3 +85,9 @@
 	<ListItems />
 {/if}
 <Modal show={showModal} />
+
+<style>
+	.hide {
+		display: none;
+	}
+</style>
