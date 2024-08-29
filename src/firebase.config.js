61b85637ebf9380d8575c72579fa9f9af3e86e79
CoderRaph1 <raphael02@hotmail.co.uk>
2024-08-29 11:44:44 +0100

    config changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,32 +3,34 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
-export const firebaseConfig = {
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASECONFIG);
+  
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
+  export const auth = getAuth(app)
+  export default app
+  
 
-  apiKey: "AIzaSyA_QNyCRENp5l9nUwHvp9HkP77H0dknE9g",
-
-  authDomain: "meal-planner-fe-database.firebaseapp.com",
-
-  projectId: "meal-planner-fe-database",
-
-  storageBucket: "meal-planner-fe-database.appspot.com",
-
-  messagingSenderId: "372847420168",
-
-  appId: "1:372847420168:web:bf896c8df182875b3142bd",
-
-  measurementId: "G-G2D5W6SEQ5"
-
-};
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app)
-export default app
-
-
-
-
-
+  
+  // export const firebaseConfig = {
+  
+  //   apiKey: "AIzaSyA_QNyCRENp5l9nUwHvp9HkP77H0dknE9g",
+  
+  //   authDomain: "meal-planner-fe-database.firebaseapp.com",
+  
+  //   projectId: "meal-planner-fe-database",
+  
+  //   storageBucket: "meal-planner-fe-database.appspot.com",
+  
+  //   messagingSenderId: "372847420168",
+  
+  //   appId: "1:372847420168:web:bf896c8df182875b3142bd",
+  
+  //   measurementId: "G-G2D5W6SEQ5"
+  
+  // };
+  
+  
+  
